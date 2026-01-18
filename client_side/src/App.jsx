8f3e1b98c486346511dd87b733e86810f158c876
dewansh <dewanshbhojak123@gmail.com>
2026-01-18 20:38:10 +0530

    navbar finished

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './component/Login'
 import Register from './component/Register'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './component/Home'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Login/>}/>
        <Route path="/register" element={<Register/>}/>
+       <Route path="/home" element={<Home/>}/>
     </Routes>
    
   )
---
+++ client_side/src/component/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaUser, FaSearch, FaShoppingBag } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+   <div>
+  <nav className="w-full border-b-1 shadow-2xl px-6 py-5 flex items-center ">
+
+ 
+    <div className="flex-1">
+      <h1 className="text-3xl font-semibold">
+        Ecommerce
+      </h1>
+    </div>
+
+   
+    <div className="flex-1 flex justify-center space-x-6">
+      <a className="text-gray-800 text-lg font-medium hover:text-black hover:underline" href="#">Home</a>
+      <a className="text-gray-800 text-lg font-medium hover:text-black hover:underline" href="#">Items</a>
+      <a className="text-gray-800 text-lg font-medium hover:text-black hover:underline" href="#">Product</a>
+      <a className="text-gray-800 text-lg font-medium hover:text-black hover:underline" href="#">Contact</a>
+    </div>
+
+    
+    <div className="flex-1 flex justify-end items-center space-x-5">
+     <Link to={"/"}> <FaSearch size={20} /> </Link>
+      <FaUser size={21} />
+      <FaShoppingBag size={21} />
+    </div>
+
+  </nav>
+</div>
+
+  )
+}
+
+export default Navbar
