bca7632549020f5c8cc55bb018f4c16df5812e42
dewansh <dewanshbhojak123@gmail.com>
2026-08-13 16:26:16 +0530

    fix vercel react router routes

--- src/component/OrderSummary.jsx
+++ src/component/OrderSummary.jsx
@@ -13,7 +13,7 @@ const navigate = useNavigate();
       console.log(orderid);
 
       const options = {
-        key: import.meta.env.VITE_RAZORPAY_KEY_ID || "rzp_test_T6aWVQ3c0fO67L",
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
 
         amount: total * 100,
 
